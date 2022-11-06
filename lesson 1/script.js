43372efc73ca581080d315f8722b3a63ec0cb69f
Wladyslaw <vladislavhrabryk@gmail.com>
2022-11-06 12:18:09 +0100

    Zadanie 5,6,7

--- lesson 1/script.js
+++ lesson 1/script.js
@@ -1,19 +1,24 @@
 // Zadanie 1.
+
  console.log(123 == '123');
 console.log(123 === '123');
 
 // Zadanie 2.
+
 console.log(0 == false);
 console.log(0 === false);
 
 
 // Zadanie 3.
+
 let userName = 'Wladyslaw';
 let age = '25';
 let city = 'Gdansk';
 
 console.log(`Our user's name is: ${userName} and is ${age} years old and lives in ${city} `); */
 // Zadanie 4.
+
+
  function sum(number1,number2) {
    return number1 + number2;
 }
@@ -24,3 +29,26 @@ let myAddedNumbers = sum(ten,four);
 console.log(myAddedNumbers);
 
 
+// Zadanie 5.
+ console.log(`Username: ${userName} have length:${userName.length}`);
+
+// Zadanie 6.
+let firstUser = 'Docep';
+let seconderUser = 'Baben';
+
+if (firstUser === seconderUser) {
+console.log("O! The names are the same!");
+} else {
+    console.log("Unfortunately the names are different :(");
+}
+
+// Zadanie 7.
+let ageUser = 2;
+
+if (ageUser >= 18) {
+    console.log("User is adult");
+} else {
+    console.log("User is underage");
+}
+
+const checkMyAge = ageUser >= 18 ? console.log("User is adult") : console.log("User is underage"); 
