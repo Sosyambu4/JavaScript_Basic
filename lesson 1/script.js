dfdebdfaf4204e1c5961832e7ea8355ab78a8e32
Wladyslaw <vladislavhrabryk@gmail.com>
2022-11-06 12:12:11 +0100

    podstawy JS

---
+++ lesson 1/script.js
@@ -0,0 +1,26 @@
+// Zadanie 1.
+ console.log(123 == '123');
+console.log(123 === '123');
+
+// Zadanie 2.
+console.log(0 == false);
+console.log(0 === false);
+
+
+// Zadanie 3.
+let userName = 'Wladyslaw';
+let age = '25';
+let city = 'Gdansk';
+
+console.log(`Our user's name is: ${userName} and is ${age} years old and lives in ${city} `); */
+// Zadanie 4.
+ function sum(number1,number2) {
+   return number1 + number2;
+}
+let ten = sum(5,5);
+let four = sum(2,2);
+let myAddedNumbers = sum(ten,four);
+
+console.log(myAddedNumbers);
+
+
